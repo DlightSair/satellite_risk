b5c38531bae85c3450471032cb3812f80dd77f43
unknown <amanpaudel576@gmail.com>
2026-08-30 00:59:54 +0530

    Update TLE data

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-import "dotenv/config";
-import { app } from "./src/app.js";
-import { env } from "./src/env.js";
-
-app.listen(env.port, () => {
-  console.log(`API listening on port ${env.port}`);
-});
